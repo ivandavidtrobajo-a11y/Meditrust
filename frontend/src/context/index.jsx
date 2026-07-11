ce33bad7dfaa9531cf3c06b7c093443e8cb9399e
ivandavidtrobajo-a11y <ivandavidtrobajo@gmail.com>
2026-07-11 13:19:32 -0600

    Update index.jsx

--- frontend/src/context/index.jsx
+++ frontend/src/context/index.jsx
@@ -4,6 +4,7 @@ import { createContext, useContext, useReducer, useMemo, useState, useEffect } f
 
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 
 // Material Dashboard 2 React main context
